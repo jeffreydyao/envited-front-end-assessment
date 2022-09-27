fb4eef3e75d32e94fb492b637fb768f3194f460c
Jeffrey Yao <jeffrey.yao@dijgtal.com>
2022-09-27 21:41:50 +1000

    feat: initial landing page styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,37 @@
-import { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/future/image"
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
         <title>Binvited</title>
-        <meta name="description" content="The next best thing since Envited" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='The next best thing since Envited' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main>
-        <h1 className="text-red-700">Imagine if Snapchat had events.</h1>
-        <p>
-          Easily host and share events with your friends across any social
-          media.
-        </p>
+      <main className='flex flex-col items-center justify-center h-screen px-9 gap-9 bg-gradient-to-b from-secondary-purple-3 to-secondary-purple-2'>
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-primary-text-purple-dark text-4xl font-bold text-center'>
+            Imagine if
+            <br />
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-primary-envited-purple to-primary-envited-pink'>
+              Snapchat
+            </span>
+            <br />
+            had events.
+          </h1>
+          <p className='font-light text-center'>
+            Easily host and share events with your friends across any social media.
+          </p>
+        </div>
+        <img src='/app.svg' className='w-40' />
       </main>
 
       <footer></footer>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
